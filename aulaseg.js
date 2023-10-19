9bbc5a83d26571fc1d1cc4c73ebe198fb89d9ce4
TamiresRosauro <141837326+TamiresRosauro@users.noreply.github.com>
2023-10-18 22:13:51 -0300

    Adicionando os arquivos da aula

---
+++ aulaseg.js
@@ -0,0 +1,21 @@
+
+// const numero = prompt("Digite um número:")
+
+// function parOuImpar(numero) {
+//     if (numero % 2 === 0) {
+//         mensagem = "O número é par"
+//     }
+//     else if (numero % 2 != 0) {
+//         mensagem = "O número não é par"
+//     }
+
+// }
+// console.log(mensagem)
+
+
+function tabuada (){
+    let numero = *5
+      for(let i = 0; i < 10; i++)    
+
+
+}
